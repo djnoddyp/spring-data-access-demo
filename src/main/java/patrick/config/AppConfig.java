425064b4bb0ccf0a11512ce07fc5573f6d83cb34
Patrick <pnodder@gmail.com>
2017-10-07 12:38:16 +0100

    Add comment

--- src/main/java/patrick/config/AppConfig.java
+++ src/main/java/patrick/config/AppConfig.java
@@ -56,6 +56,7 @@ public class AppConfig {
         return props;
     }
 
+    // Needed for Hibernate
     @Bean
     @Autowired
     public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
